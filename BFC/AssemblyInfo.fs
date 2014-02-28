358fa28c510c33896d27b5253f0378b39147c79d
J. Wostenberg <jwosty@me.fake>
2014-02-27 21:42:54 -0700

    Initial commit. I have a lexer/parser working right now, but that\'s all... I promise\!

---
+++ BFC/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+﻿module BFC.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("BFC")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("John Wostenberg")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
