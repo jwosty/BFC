340f2cc25df0931d574bb163ca39c1bb1f2a08f1
John Wostenberg <jwosty@users.noreply.github.com>
2021-04-16 17:14:15 -0500

    Convert template to an embedded resource

--- BF.Compiler/Program.fs
+++ BF.Compiler/Program.fs
@@ -2,6 +2,7 @@
 open BF.Common
 open System
 open System.IO
+open System.Reflection
 open System.Text.RegularExpressions
 open Argu
 
@@ -19,53 +20,26 @@ type CliArgs =
             | InFile _ -> "Specify an input source file"
             | OutFile _ -> "Specify an output source file"
 
-//type Platform = | CSource
-
-//type Options =
-//  { platform: Platform
-//    inFile: string
-//    outFile: string }
-
-//let platformFromName (name: string) =
-//  match name.ToLowerInvariant () with
-//  | "c" -> CSource
-//  | _ ->
-//    eprintfn "Unknown platform: `%s'" name
-//    exit 1
-
-//let printUsage () =
-//  printfn "Usage: bfc <source file> <options>"
-
-//let printHelp (optionSet: OptionSet) =
-//  optionSet.WriteOptionDescriptions Console.Out
-
-//let parseArgs (args: string []) =
-//  let optionSet = new OptionSet()
-  
-//  let platform = ref CSource
-//  let outFile = ref "a.out"
-//  let inFile = ref ""
-  
-//  optionSet.Add("i=|infile=", "Source file to read.",
-//    (fun f -> inFile := f)) |> ignore
-//  optionSet.Add("p=|platform=", "Compile targeting this platform. Currently, the only platform is C source which is then compiled using GCC.",
-//    (fun p -> platform := (platformFromName p))) |> ignore
-//  optionSet.Add("o=|outfile=", "Write output to this file.",
-//    (fun f -> outFile := f)) |> ignore
-//  let inFile =
-//    if args.Length = 0 || (args.[0].[0] = '-')
-//      then
-//        printUsage()
-//        printHelp optionSet
-//        exit 1
-//      else ref args.[0]
-//  let args = optionSet.Parse args
-  
-//  { platform = !platform
-//    inFile = !inFile
-//    outFile = !outFile }
-
 let argParser = ArgumentParser.Create<CliArgs>()
+
+let mainCompilation inFile outFile = async {
+    let assembly = typeof<CliArgs>.Assembly
+    printfn "resources: %A" (assembly.GetManifestResourceNames ())
+
+    let templateName = $"{assembly.GetName().Name}.template.c"
+    //printfn "searching for %s" name
+    use templateReader = new StreamReader(assembly.GetManifestResourceStream templateName)
+    let! template = Async.AwaitTask (templateReader.ReadToEndAsync ())
+    //printfn "%A" template
+    let bDump =
+        inFile |> File.ReadAllText
+        |> parse |> toIR |> optimize
+        |> To.CSource 1
+    let cSource = Regex.Replace(template, "  /// --- BF CODE --- ///", bDump)
+    //printfn "cSrc:\n%s" cSource
+    To.C cSource outFile
+}
+    
 
 [<EntryPoint>]
 let main args =
@@ -76,14 +50,7 @@ let main args =
         let inFile = cliArgs.GetResult <@ InFile @>
         let outFile = cliArgs.GetResult <@ OutFile @>
 
-        let template = File.ReadAllText("template.c")
-        let bDump =
-            inFile |> File.ReadAllText
-            |> parse |> toIR |> optimize
-            |> To.CSource 1
-        let cSource = Regex.Replace(template, "  /// --- BF CODE --- ///", bDump)
-        printfn "cSrc:\n%s" cSource
-        To.C cSource outFile
+        Async.RunSynchronously (mainCompilation inFile outFile)
         0
     with
         | :? ArguParseException ->
