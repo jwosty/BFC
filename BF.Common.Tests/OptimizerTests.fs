fb351ba11b8358bd0a7c4de3962d414a87a1099d
John Wostenberg <jwosty@users.noreply.github.com>
2021-04-19 11:19:17 -0500

    Group opt tests by some new modules

--- BF.Common.Tests/OptimizerTests.fs
+++ BF.Common.Tests/OptimizerTests.fs
@@ -6,139 +6,135 @@ open BF
 open FsUnit.Xunit
 open Xunit
 
-[<Fact>]
-let ``Given a sequence of +`` () =
-    "+++++"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5)]
+module InstructionSequences =
+    [<Fact>]
+    let ``Given a sequence of +`` () =
+        "+++++"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5)]
 
-[<Fact>]
-let ``Given a sequence of >>`` () =
-    ">>>"
-    |> parse |> toIR |> optimize
-    |> should equal [AddPtr 3]
+    [<Fact>]
+    let ``Given a sequence of >>`` () =
+        ">>>"
+        |> parse |> toIR |> optimize
+        |> should equal [AddPtr 3]
 
-[<Fact>]
-let ``Given a long sequence of +`` () =
-    "++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++
-     ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++
-     ++++++++++ ++++++++++ ++++++++++ ++++++++++"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 140)]
+    [<Fact>]
+    let ``Given a long sequence of +`` () =
+        "++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++
+         ++++++++++ ++++++++++ ++++++++++ ++++++++++ ++++++++++
+         ++++++++++ ++++++++++ ++++++++++ ++++++++++"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 140)]
 
-[<Fact>]
-let ``Given a combination of atom sequences`` () =
-    "++++>>+++<<<-->[--]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 4); AddPtr 2; AddCell (0, 3); AddPtr -3; AddCell (0, -2); AddPtr 1; WhileNonzero [AddCell (0, -2)]]
+    [<Fact>]
+    let ``Given a combination of atom sequences`` () =
+        "++++>>+++<<<-->[--]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 4); AddPtr 2; AddCell (0, 3); AddPtr -3; AddCell (0, -2); AddPtr 1; WhileNonzero [AddCell (0, -2)]]
 
-[<Fact>]
-let ``Given a comment loop at the beginning of a program`` () =
-    "[Hello, world. This loop body can never be executed, so we have ourselves a makeshift comment here. We can write
-      whatever we want in here! </comment>] +++++[-]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); ClearCell]
+module SimpleLoops =
+    [<Fact>]
+    let ``Given a comment loop at the beginning of a program`` () =
+        "[Hello, world. This loop body can never be executed, so we have ourselves a makeshift comment here. We can write
+          whatever we want in here! </comment>] +++++[-]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); ClearCell]
 
-[<Fact>]
-let ``Given a clear loop`` () =
-    "+[-]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 1); ClearCell]
+    [<Fact>]
+    let ``Given a clear loop`` () =
+        "+[-]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 1); ClearCell]
 
-[<Fact>]
-let ``Given a clear loop inside a more complex loop`` () =
-    "++>+++>++[>[-]<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 2); AddPtr 1; AddCell (0, 3); AddPtr 1; AddCell (0, 2); WhileNonzero [AddPtr 1; ClearCell; AddPtr -2]]
+    [<Fact>]
+    let ``Given a clear loop inside a more complex loop`` () =
+        "++>+++>++[>[-]<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 2); AddPtr 1; AddCell (0, 3); AddPtr 1; AddCell (0, 2); WhileNonzero [AddPtr 1; ClearCell; AddPtr -2]]
 
-[<Fact>]
-let ``Given a + clear loop`` () =
-    "+[+]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 1); ClearCell]
+    [<Fact>]
+    let ``Given a + clear loop`` () =
+        "+[+]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 1); ClearCell]
 
-[<Fact>]
-let ``Given a + clear loop inside a more complex loop`` () =
-    "++>+++>++[>[+]<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 2); AddPtr 1; AddCell (0, 3); AddPtr 1; AddCell (0, 2); WhileNonzero [AddPtr 1; ClearCell; AddPtr -2]]
+    [<Fact>]
+    let ``Given a + clear loop inside a more complex loop`` () =
+        "++>+++>++[>[+]<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 2); AddPtr 1; AddCell (0, 3); AddPtr 1; AddCell (0, 2); WhileNonzero [AddPtr 1; ClearCell; AddPtr -2]]
 
-[<Fact>]
-let ``Given a simple move loop`` () =
-    "+++++[->+<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [1,1]]
+module MoveMulLoops =
+    [<Fact>]
+    let ``Given a simple move loop`` () =
+        "+++++[->+<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [1,1]]
 
-[<Fact>]
-let ``Given a move loop with a non-adjacent cell`` () =
-    "+++++[->>+<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [2,1]]
+    [<Fact>]
+    let ``Given a move loop with a non-adjacent cell`` () =
+        "+++++[->>+<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [2,1]]
 
-[<Fact>]
-let ``Given a move loop with a distant cell`` () =
-    "+++++[->>>>>+<<<<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [5,1]]
+    [<Fact>]
+    let ``Given a move loop with a distant cell`` () =
+        "+++++[->>>>>+<<<<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [5,1]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with a factor of 3`` () =
-    "+++++[->+++<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [1,3]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with a factor of 3`` () =
+        "+++++[->+++<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [1,3]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with a factor of 6 and non-adjacent cells`` () =
-    "+++++[->>>++++++<<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [3,6]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with a factor of 6 and non-adjacent cells`` () =
+        "+++++[->>>++++++<<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [3,6]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with iterator decrement at end of loop `` () =
-    "+++++[>>++++<<-]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [2,4]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with iterator decrement at end of loop `` () =
+        "+++++[>>++++<<-]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [2,4]]
 
-//[<Fact>]
-//let ``Given a move-and-multiply loop with multiple destinations`` () =
-//    "+++++[->>+++++>+++>++<<<<]"
-//    |> parse |> toIR |> optimize
-//    |> should equal [AddCell (0, 5); MoveMulCell [2,5; 3,3; 4,2]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with two destinations`` () =
+        "+++++[->+++++>+++<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [1,5;2,3]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with two destinations`` () =
-    "+++++[->+++++>+++<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [1,5;2,3]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with three destinations`` () =
+        "+++++[->+++++>+++>++<<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [1,5;2,3;3,2]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with three destinations`` () =
-    "+++++[->+++++>+++>++<<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [1,5;2,3;3,2]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with three non-adjacent destinations`` () =
+        "+++++[->+++++>>+++>>++<<<<<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [1,5;3,3;5,2]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with three non-adjacent destinations`` () =
-    "+++++[->+++++>>+++>>++<<<<<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [1,5;3,3;5,2]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with two non-adjacent destinations, using cells both to the left and to the right`` () =
+        "+++++[-<<+++++>>>+++<]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [-2,5;1,3]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with two non-adjacent destinations, using cells both to the left and to the right`` () =
-    "+++++[-<<+++++>>>+++<]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [-2,5;1,3]]
+    [<Fact>]
+    let ``Given a move-and-multiply loop with two non-adjacent destinations, using cells both to the left and to the right, and with the decrementer at the end`` () =
+        "+++++[<<+++++>>>+++<-]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 5); MoveMulCell [-2,5;1,3]]
 
-[<Fact>]
-let ``Given a move-and-multiply loop with two non-adjacent destinations, using cells both to the left and to the right, and with the decrementer at the end`` () =
-    "+++++[<<+++++>>>+++<-]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 5); MoveMulCell [-2,5;1,3]]
-
-[<Fact>]
-let ``Given a move-and-multiply loop with a very distant destination cell`` () =
-    "+[>>>>>>>[-<<<<<<<<<<<+++++>>>>>>>>>>>]>>>]"
-    |> parse |> toIR |> optimize
-    |> should equal [AddCell (0, 1); WhileNonzero [AddPtr 7; MoveMulCell [-11,5]; AddPtr 3]]
-
+    [<Fact>]
+    let ``Given a move-and-multiply loop with a very distant destination cell`` () =
+        "+[>>>>>>>[-<<<<<<<<<<<+++++>>>>>>>>>>>]>>>]"
+        |> parse |> toIR |> optimize
+        |> should equal [AddCell (0, 1); WhileNonzero [AddPtr 7; MoveMulCell [-11,5]; AddPtr 3]]
 
